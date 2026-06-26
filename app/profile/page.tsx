3c58181670263d8ee38ec3e59631612c323bbd39
Wyked Samurai <Pjcarter4252@gmail.com>
2026-06-26 13:01:26 -0400

    Render document library

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,6 @@
+import ResumeDocumentLibrary from "./ResumeDocumentLibrary";
 import UnifiedProfileWorkspace from "./UnifiedProfileWorkspace";
 
 export default function ProfilePage() {
-  return <UnifiedProfileWorkspace />;
+  return <><UnifiedProfileWorkspace /><ResumeDocumentLibrary /></>;
 }
